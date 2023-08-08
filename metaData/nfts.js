ac911d7b0d9cef712bc9d7c296715a34ae4ea8cd
SK-4518 <136696857+SK-4518@users.noreply.github.com>
2023-08-08 21:05:43 +0530

    nfts.js

---
+++ metaData/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "india",
+    image: "QmV3ZLeJWP7BvsfrKTFubAp5FRoAU2hLxcjt2W1Pcwcb5y",
+  },
+  {
+    name: "1",
+    description: "america",
+    image: "QmehVsPowjcvCG9QqAVKUiDDz6SfZb2LrSctjqKz3vca7L",
+  }, //change
+  {
+    name: "2",
+    description: "singapore",
+    image: "Qmbnbw9Rx7LDAgtN8eJYTZjHjD1b8vaT1n1RoBAhae1axb",
+  },
+  {
+    name: "3",
+    description: "australia",
+    image: "QmQDrLpRHf1qmsiRo29vv82qtxq8UKVWY6qzmDa1YLtoY8",
+  },
+  {
+    name: "4",
+    description: "Europe",
+    image: "QmfVTkU4gXsnfTY55XShg8xZXGDiLBEUR3BgB5LFqnHXrk",
+  },
+];
+
+module.exports = nfts;
